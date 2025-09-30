f7c5e42c02a54a2adc7afcbd731c77e012b524da
Victor Hugo Ribeiro <ribeiro.santos.victor3007@escola.pr.gov.br>
2025-09-30 09:52:55 -0300

    sla

---
+++ app/src/teste/janela.java
@@ -0,0 +1,10 @@
+package teste;
+
+public class janela {
+    public static void main(String[] args) {
+        /*JFrame janela= new JFrame("Titulo da Janela");
+
+        janela.setBounds(0,0,1920,1080);
+        janela.setVisible(true);*/
+    }
+}
